0009118b3ecf795684adb8c0326ab9f3b96c8262
Ricardo Moura <ricardomoura1979@gmail.com>
2022-06-29 19:00:32 +0000

    updated

--- queries/3_property_listings_by_city.sql
+++ queries/3_property_listings_by_city.sql
@@ -1,7 +1,7 @@
 SELECT properties.*, AVG(property_reviews.rating) AS average_rating
 FROM properties
 JOIN property_reviews ON property_reviews.property_id = properties.id
-WHERE city LIKE '%ancouv%'
+WHERE city LIKE '%ont%'
 GROUP BY properties.id
 HAVING avg(property_reviews.rating) >= 4
 ORDER BY cost_per_night
